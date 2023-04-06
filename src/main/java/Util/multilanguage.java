5231ac8a9f93c7000b64c9c6d1c7725307d85c43
manas <behera@gmail.com>
2023-04-06 18:40:59 +0530

    4th commit

--- src/main/java/Util/multilanguage.java
+++ src/main/java/Util/multilanguage.java
@@ -1,7 +1,14 @@
 package Util;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
 public class multilanguage {
-  int a=5;
-  int b=6;
-  int c=a+b;
+  public static void main(String[] args) {
+    int a=5;
+    int b=6;
+    int c=a+b;
+    System.out.println(c);
+  }
+
+
 }
